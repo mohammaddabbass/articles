be443c7e68911ba940ae0d141d61bae93119e32c
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-03-08 22:19:32 +0200

    register api implemented

--- articles-server/user/v1/register.php
+++ articles-server/user/v1/register.php
@@ -1,5 +1,56 @@
 <?php 
+require '../../connection/connection.php';
 include '../../models/User.php';
-require_once __DIR__ . '/UserSkeleton.php';
+require_once  '../../models/UserSkeleton.php';
+
+if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(["message" => "Invalid Request Method"]);
+    exit();
+} else {
+    if(empty($_POST['email']) || empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['password'])) {
+        http_response_code(401);
+        echo json_encode(["message" => "please fill all the required fields"]);
+        exit();
+    }
+
+    $email = $_POST['email'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $password = $_POST['password'];
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $user = new User($conn);
+    $userRegiter = $user->getUser($email);
+
+    if($userRegiter) {
+        echo json_encode(["message" => "email already exists"]);
+        exit();
+    }
+
+    $userSkeleton = new UserSkeleton(null, $email, $first_name, $last_name, $hashed_password, date('Y-m-d H:i:s'));
+
+    if($user->createUser($userSkeleton)) {
+        $userDetails = $user->getUser($email);
+        $response = [
+            "message" => "User registered successfully",
+            "user" => [
+                "id" => $userDetails->getUserId(),  
+                "email" => $userDetails->getEmail(),
+                "first_name" => $userDetails->getFirstName(),
+                "last_name" => $userDetails->getLastName(),
+            ]
+        ];
+
+        http_response_code(201);
+        echo json_encode(["message" => "user registerd successfully!", $response]);
+    } else {
+        http_response_code(500);
+        echo json_encode(["message" => "Failed to register user"]);
+    }
+
+}
+
 
 ?>
